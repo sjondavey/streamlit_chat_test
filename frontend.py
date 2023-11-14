b517d57f0080c9c44049d1eb9a918df12146aae1
Steven <daveysj@gmail.com>
2023-11-14 15:25:47 +0200

    Add authentication

--- frontend.py
+++ frontend.py
@@ -8,49 +8,49 @@ st.title("ChatGPT-like clone")
 
 openai.api_key = st.secrets["OPENAI_API_KEY"]
 
-# authenticator = stauth.Authenticate(
-#     dict(st.secrets['credentials']),
-#     st.secrets['cookie']['name'],
-#     st.secrets['cookie']['key'],
-#     st.secrets['cookie']['expiry_days'],
-#     st.secrets['preauthorized']
-# )
+authenticator = stauth.Authenticate(
+    dict(st.secrets['credentials']),
+    st.secrets['cookie']['name'],
+    st.secrets['cookie']['key'],
+    st.secrets['cookie']['expiry_days'],
+    st.secrets['preauthorized']
+)
 
-# name, authentication_status, username = authenticator.login('Login', 'sidebar') # location is 'sidebar' or 'main'
+name, authentication_status, username = authenticator.login('Login', 'sidebar') # location is 'sidebar' or 'main'
 
-# if authentication_status:
+if authentication_status:
 
-if "openai_model" not in st.session_state:
-    st.session_state["openai_model"] = "gpt-3.5-turbo"
+    if "openai_model" not in st.session_state:
+        st.session_state["openai_model"] = "gpt-3.5-turbo"
 
-if "messages" not in st.session_state:
-    st.session_state.messages = []
+    if "messages" not in st.session_state:
+        st.session_state.messages = []
 
-for message in st.session_state.messages:
-    with st.chat_message(message["role"]):
-        st.markdown(message["content"])
+    for message in st.session_state.messages:
+        with st.chat_message(message["role"]):
+            st.markdown(message["content"])
 
-if prompt := st.chat_input("What is up?"):
-    st.session_state.messages.append({"role": "user", "content": prompt})
-    with st.chat_message("user"):
-        st.markdown(prompt)
+    if prompt := st.chat_input("What is up?"):
+        st.session_state.messages.append({"role": "user", "content": prompt})
+        with st.chat_message("user"):
+            st.markdown(prompt)
 
-    with st.chat_message("assistant"):
-        message_placeholder = st.empty()
-        full_response = ""
-        response = openai.chat.completions.create(
-            model=st.session_state["openai_model"],
-            messages=st.session_state.messages,
-            stream=True,
-        )
-        for part in response:
-            full_response += (part.choices[0].delta.content or "")
-            message_placeholder.markdown(full_response + "▌")
-        message_placeholder.markdown(full_response)
-    st.session_state.messages.append({"role": "assistant", "content": full_response})
+        with st.chat_message("assistant"):
+            message_placeholder = st.empty()
+            full_response = ""
+            response = openai.chat.completions.create(
+                model=st.session_state["openai_model"],
+                messages=st.session_state.messages,
+                stream=True,
+            )
+            for part in response:
+                full_response += (part.choices[0].delta.content or "")
+                message_placeholder.markdown(full_response + "▌")
+            message_placeholder.markdown(full_response)
+        st.session_state.messages.append({"role": "assistant", "content": full_response})
 
-# elif authentication_status == False:
-#     st.error('Username/password is incorrect')
-# elif authentication_status == None:
-#     st.warning('Please enter your username and password')
+elif authentication_status == False:
+    st.error('Username/password is incorrect')
+elif authentication_status == None:
+    st.warning('Please enter your username and password')
 
